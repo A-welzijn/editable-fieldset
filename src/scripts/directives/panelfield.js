ae889534a0b0684ee52ff1b9730e6814ec62cfd1
vincentbouillart <vincentbouillart@live.be>
2016-01-12 08:58:13 +0100

    add extra fields to combine fields

--- src/scripts/directives/panelfield.js
+++ src/scripts/directives/panelfield.js
@@ -64,7 +64,8 @@
 						national:'tink-national-number',
 						datepickerRange:'tink-datepicker-range',
 						tinkIdentity:'tink-identity-number',
-						textarea:'textarea'
+						textarea:'textarea',
+						awelzijnCombine:'awelzijn-combine'
 					}
 					var calculated = {};
 					for(var i=0;i<Object.keys(editableFields).length;i++){
