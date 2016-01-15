7d80c69e08a599f87a7224598335c6adf95cf6be
vincentbouillart <vincentbouillart@live.be>
2016-01-15 14:57:29 +0100

    editabel fieldset nieuwe insteek

--- src/scripts/directives/panelfield.js
+++ src/scripts/directives/panelfield.js
@@ -1,4 +1,3 @@
-'use strict';
 (function (module) {
 	try {
 		module = angular.module('awelzijn.editableFieldset');
@@ -19,179 +18,157 @@
 				ngClick: '&'
 			},
 			templateUrl: 'templates/panelfield.html',
-			link: function ($scope, element, attrs,transclude) {
-				//$scope.editMode = false;
-				var originalElement = $(element).clone();
-				if ($scope.colspan) {
-					$scope.colspanClass = ("col-lg-@ col-md-@").replace(/@/g, $scope.colspan);
-				} else {
-					$scope.colspanClass = ("col-lg-@ col-md-@").replace(/@/g, '6');
-				}
+			compile:function(){
+				return {
+					post: function ($scope, element, attrs,transclude) {
+						var _addedObjs = [];
+						//$scope.editMode = false;
+						var originalElement = $(element).clone();
+						if ($scope.colspan) {
+							$scope.colspanClass = ("col-lg-@ col-md-@").replace(/@/g, $scope.colspan);
+						} else {
+							$scope.colspanClass = ("col-lg-@ col-md-@").replace(/@/g, '6');
+						}
 
 
-				var formElement = element.closest('[tink-fieldset]');
-				var edit = findEditableFields(element.find('[ng-transclude]'));				
+						var formElement = element.closest('[tink-fieldset]');
+						var edit = findEditableFields(element.find('[ng-transclude]'));				
 
-				$scope.$watch(function(){return formElement.isolateScope().tinkFormStatus},function(value,old){
-					if(old === 'mouseFocus'){
-						setFakeInput();
-					}else if(value === 'mouseFocus'){
-						setRealInput();					
-					}
-				});
+						$scope.$watch(function(){return formElement.isolateScope().tinkFormStatus},function(value,old){
+							if(old === 'mouseFocus'){
+								setFakeInput();
+							}else if(value === 'mouseFocus'){
+								setRealInput();					
+							}
+						});
+						
+						function addToObject(value,type,object,showField,isolateScope){
+							if(showField){
+								fake = showField;
+							}else{
+								var fake = $('<div contenteditable="true" class="faux-input">{{label || "-"}}</div>');
+								fake = $compile(fake)($scope);
+							}
+							$(fake).insertAfter($(value));
+							$(value).css('display','none');
+							_addedObjs.push({fake:$(fake),real:$(value)});
 
-				//This creates the objects we are going to need !
-				function addToObject(value,type,object,showField){
-
-					var fake,
-					scope;
-					if(showField){
-						fake = showField;
-						scope = $(value).scope();
-					}else{
-						fake = $('<div contenteditable="true" class="faux-input">{{label || "-"}}</div>');
-						scope = $(value).scope();
-					}					
-					
-					if(object[type] && object[type] instanceof Array){
-						object[type].push({real:value,fake:fake,scope:scope});
-					}else{
-						object[type] = [{used:{real:undefined,fake:undefined},real:value,fake:fake,scope:scope}];
-					}
-					return object;
-				}
-
-				//this finds the fields we want to adjust
-				function findEditableFields(element){
-					var editableFields = {
-						input:'input',
-						select:'select',
-						datepicker:'data-tink-datepicker',
-						timepicker:'tink-timepicker',
-						national:'tink-national-number',
-						datepickerRange:'tink-datepicker-range',
-						tinkIdentity:'tink-identity-number',
-						textarea:'textarea',
-						awelzijnTransclude:'awelzijn-transclude'
-					}
-					var calculated = {};
-					for(var i=0;i<Object.keys(editableFields).length;i++){
-						var currentKey = Object.keys(editableFields)[i],
-							currentValue = editableFields[currentKey];
-							var childs = $(element).children();
-								for(var j=0;j<childs.length;j++){
-									var child = $(childs[j]);
-									if(child.is(currentValue) && editableFields.awelzijnTransclude === currentValue){
-										var fake = child.find('transclude-show').clone();
-										var real = child.find('transclude-edit').clone();
-										element.replaceWith(real);
-										calculated = addToObject(real,currentKey,calculated,fake)										
-									}else if(child.is(currentValue)){
-										calculated = addToObject(child,currentKey,calculated,undefined)
+							fake.bind('mousedown click touchstart',function(){
+								$timeout(function(values){
+									if($(value).is('transclude-edit')){
+										focusin(value);
+										formElement.isolateScope().setClassActive("mouseFocus",null);
+									}else{
+										focusin(value);
 									}
-								}
-							
-					}
-					return calculated;
-				}
+								},150);
+								return false;	
+							});
 
-				//Function to replace the fake fields with the right ones
-				function setRealInput(){
-					for(var i=0;i<Object.keys(edit).length;i++){
-						var currentKey = Object.keys(edit)[i],
-							currentValue = edit[currentKey];
-						for(var j = 0;j<currentValue.length;j++){
-							var usedFake = currentValue[j].used.fake,
-							usedReal = currentValue[j].used.real,
-							scopeField = currentValue[j].scope,
-							real = $(currentValue[j].real).clone();
-							if(usedFake === undefined && usedReal === undefined){
-								usedFake = currentValue[j].fake;
-								usedReal = $(currentValue[j].real).clone();
-							}
-							currentValue[j].used.real = real;
-							currentValue[j].used.fake = usedFake;
-
-							$(usedFake).replaceWith(real);
 							if(formElement && formElement.isolateScope()){
-								formElement.isolateScope().addEvents($(real));
-							}						
-							$compile(real)(scopeField);
-						}
-					}
-
-				}
-
-				//Hack to open the selectbox
-				var openSelect = function(selector,type){
-				     var element = $(selector)[0], worked = false;
-				    if (document.createEvent) { // all browsers
-				        var e = document.createEvent("MouseEvents");
-				        e.initMouseEvent(type, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-				        worked = element.dispatchEvent(e);
-				    } else if (element.fireEvent) { // ie
-				        worked = element.fireEvent("onmousedown");
-				    }
-				}
-
-				function isDisabled(element){
-					element = $(element);
-					 return element.attr('disabled') || element.attr('is-disabled') || element.attr('data-is-disabled') || element.attr('data-disabled');
-				}
-				
-				function setFakeInput(){
-					for(var i=0;i<Object.keys(edit).length;i++){
-						var currentKey = Object.keys(edit)[i],
-							currentValue = edit[currentKey];
-						for(var j = 0;j<currentValue.length;j++){
-
-							var usedFake = currentValue[j].used.fake,
-							usedReal = currentValue[j].used.real,
-							scopeField = currentValue[j].scope,
-							fake = $(currentValue[j].fake).clone();
-							if(usedFake === undefined && usedReal === undefined){
-								usedReal = currentValue[j].real;
-								usedFake = $(currentValue[j].fake).clone();
+								formElement.isolateScope().addEvents($(value));
 							}
-
-							formElement.isolateScope().removeEvents($(usedReal));
-								
-							currentValue[j].used.real = usedReal;
-							currentValue[j].used.fake = fake;
-							$(usedReal).replaceWith($(fake))
-
-							$compile(fake)($scope);
-								fake.bind('mousedown click touchstart',{i: currentKey,j:j},function(evt){
-									$timeout(function(values){
-										var real = edit[values.i][values.j].used.real;
-										if(real === undefined){
-											real = edit[values.i][values.j].real;
-										}
-										if($(real).is('transclude-edit')){
-											formElement.isolateScope().setClassActive("mouseFocus",null);
-										}else{
-											focusin(real);
-										}
-									},150, true, {i:evt.data.i,j:evt.data.j})								
-								})
-							formElement.isolateScope().addEvents($(fake));							
 						}
+
+						function setRealInput(){
+							for(var i=0;i<_addedObjs.length;i++){
+								var elem = _addedObjs[i];
+								if(elem.fake){
+									elem.fake.css('display','none');
+								}
+								if(elem.real){
+									elem.real.css('display','block');
+								}
+							}
+						}
+
+						function setFakeInput(){
+							for(var i=0;i<_addedObjs.length;i++){
+								var elem = _addedObjs[i];
+								if(elem.fake){
+									elem.fake.css('display','block');
+								}
+								if(elem.real){
+									elem.real.css('display','none');
+								}
+							}	
+						}
+
+
+
+						//this finds the fields we want to adjust
+						function findEditableFields(element){
+							var editableFields = {
+								input:'input',
+								select:'select',
+								datepicker:'data-tink-datepicker',
+								timepicker:'tink-timepicker',
+								national:'tink-national-number',
+								datepickerRange:'tink-datepicker-range',
+								tinkIdentity:'tink-identity-number',
+								textarea:'textarea',
+								awelzijnTransclude:'awelzijn-transclude',
+								digipolisCodetabelDropdown:'digipolis-codetabel-dropdown'
+							}
+							var calculated = {};
+							for(var i=0;i<Object.keys(editableFields).length;i++){
+								var currentKey = Object.keys(editableFields)[i],
+									currentValue = editableFields[currentKey];
+									var childs = $(element).children();
+										for(var j=0;j<childs.length;j++){
+											var child = $(childs[j]);
+											if(child.is(currentValue) && editableFields.awelzijnTransclude === currentValue){
+												var fake = child.find('transclude-show');
+												var real = child.find('transclude-edit');
+												element.replaceWith(real);
+												var isolateScope = child.find('transclude-edit').isolateScope()
+												calculated = addToObject(real,currentKey,calculated,fake,isolateScope);
+											}else if(child.is(currentValue)){
+												calculated = addToObject(child,currentKey,calculated,undefined)
+											}
+										}
+								
+							}
+							return calculated;
+						}
+
+						//Hack to open the selectbox
+						var openSelect = function(selector,type){
+						     var element = $(selector).find('select')[0], worked = false;
+						     if(!element){
+						     	if($(selector)[0]){
+						     		element = (selector)[0];
+						     	}
+						     }
+						    if (element && document.createEvent) { // all browsers
+						        var e = document.createEvent("MouseEvents");
+						        e.initMouseEvent(type, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+						        worked = element.dispatchEvent(e);
+						    } else if (element && element.fireEvent) { // ie
+						        worked = element.fireEvent("onmousedown");
+						    }
+						}
+
+						function isDisabled(element){
+							element = $(element);
+							 return element.attr('disabled') || element.attr('is-disabled') || element.attr('data-is-disabled') || element.attr('data-disabled');
+						}
+						
+						function focusin(real){
+							$(real).focusin();
+							$(real).focus();
+							openSelect($(real),'focus');
+							openSelect($(real),'mousedown');
+						}
+
+						$scope.hasClickCallback = function () {
+							return angular.isDefined(attrs.ngClick);
+						}
+						setFakeInput();
 					}
-
-				}
-
-				function focusin(real){
-					$(real).focusin();
-					$(real).focus();
-					openSelect($(real),'focus');
-					openSelect($(real),'mousedown');
-				}
-
-				$scope.hasClickCallback = function () {
-					return angular.isDefined(attrs.ngClick);
-				}
-				setFakeInput();
+						}
 			}
+			
 		};
 	}])
 })();
